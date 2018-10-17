724db020375bd697ee1c12ea91a15b9454b58471
wowKimSoHyun <hadesi1999@gmail.com>
2018-10-17 17:22:49 +0700

    update

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,30 @@
 package sample;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioMenuItem;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    @FXML
+    private Label currWord;
+    @FXML
+    private RadioMenuItem initAV;
+    @FXML
+    private RadioMenuItem initVA;
+
+    private HashMap<String,String> data = new HashMap<>();
+    private ArrayList<String> wordTarget = new ArrayList<>();
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        
+    }
 }
